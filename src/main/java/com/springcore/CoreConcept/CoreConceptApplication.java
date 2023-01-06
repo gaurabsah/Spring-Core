502869e925ceac7b3bc7e610cbf19e893064c828
Gaurab Sah <gaurab.sah.777@gmail.com>
2023-01-06 11:36:46 +0530

    Initial commit

---
+++ src/main/java/com/springcore/CoreConcept/CoreConceptApplication.java
@@ -0,0 +1,13 @@
+package com.springcore.CoreConcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreConceptApplication.class, args);
+	}
+
+}
